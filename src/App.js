67c02ce63529248e40cea6f91ecb1f50b4ffc112
Caspar Velsen-Zerweck <caspar@velsen-zerweck.de>
2022-03-25 17:11:58 +0100

    Add BrowserRoutes to index.js and fix App function

--- src/App.js
+++ src/App.js
@@ -4,13 +4,15 @@ import HomePage from './pages/HomePage';
 import FormPage from './pages/FormPage';
 
 export default function App() {
-  <>
-    <Routes>
-      <Route path="/" element={<HomePage />} />
-      <Route path="/formpage" element={<FormPage />} />
-    </Routes>
-    <footer>
-      <Navigation />
-    </footer>
-  </>;
+  return (
+    <>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/formpage" element={<FormPage />} />
+      </Routes>
+      <footer>
+        <Navigation />
+      </footer>
+    </>
+  );
 }
