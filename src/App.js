36f8865df617495ea351efbd9aff522feaa85a17
Caspar Velsen-Zerweck <caspar@velsen-zerweck.de>
2022-03-25 17:02:15 +0100

    Add test functions to pages

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import { Routes, Route } from 'react-router-dom';
 import Navigation from './components/Navigation';
+import HomePage from './pages/HomePage';
+import FormPage from './pages/FormPage';
 
 export default function App() {
   <>
     <Routes>
-      <Route />
-      <Route />
+      <Route path="/" element={<HomePage />} />
+      <Route path="/formpage" element={<FormPage />} />
     </Routes>
     <footer>
       <Navigation />
