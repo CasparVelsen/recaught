a78ce381f7205ba4f8a50e836955d709d2e4184b
Caspar Velsen-Zerweck <caspar@velsen-zerweck.de>
2022-03-27 11:52:12 +0200

    Add header to pages with styling

--- src/pages/FormPage.js
+++ src/pages/FormPage.js
@@ -1,3 +1,19 @@
+import styled from 'styled-components';
+
 export default function FormPage() {
-  return <h1>FormPage</h1>;
+  return (
+    <>
+      <Header>formpage</Header>
+      <main>
+        <h1>Create your entry:</h1>
+      </main>
+    </>
+  );
 }
+
+const Header = styled.header`
+  display: flex;
+  align-items: center;
+  height: 38px;
+  border: 1px solid black;
+`;
--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,3 +1,19 @@
+import styled from 'styled-components';
+
 export default function HomePage() {
-  return <h1>HomePage</h1>;
+  return (
+    <>
+      <Header>Homepage</Header>
+      <main>
+        <h1>Your entries:</h1>
+      </main>
+    </>
+  );
 }
+
+const Header = styled.header`
+  display: flex;
+  align-items: center;
+  height: 38px;
+  border: 1px solid black;
+`;
