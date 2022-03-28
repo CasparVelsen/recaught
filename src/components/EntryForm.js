40ac40450d9acb5f4e1989053e1b383192d45a5f
Caspar Velsen-Zerweck <caspar@velsen-zerweck.de>
2022-03-28 15:01:49 +0200

    Add first form draft

--- src/components/EntryForm.js
+++ src/components/EntryForm.js
@@ -3,23 +3,58 @@ import styled from 'styled-components';
 export default function EntryForm() {
   return (
     <div>
-      <form>
+      <Form>
         <Fieldset>
-          <div>
+          <Legend>general infos</Legend>
+          <Div>
             <label htmlFor="date">Date</label>
-            <input id="date" name="date" type="date" required />
-          </div>
-          <div>
+            <Input id="date" name="date" type="date" required />
+          </Div>
+          <Div>
             <label htmlFor="time">Time</label>
-            <input id="time" name="time" type="time" required />
-          </div>
+            <Input id="time" name="time" type="time" required />
+          </Div>
+          <Div>
+            <label htmlFor="target-species">target species</label>
+            <Input
+              id="target-species"
+              name="target-species"
+              type="text"
+              required
+            />
+          </Div>
         </Fieldset>
         <button type="submit">Submit</button>
-      </form>
+      </Form>
     </div>
   );
 }
 
+const Form = styled.form`
+  padding: 0 10px;
+`;
+
 const Fieldset = styled.fieldset`
+  display: grid;
+  grid-template-columns: 1fr 1fr;
+  gap: 20px;
+  padding: 30px 0;
   border: none;
+  border-top: 1px solid black;
+  position: relative;
+`;
+
+const Legend = styled.legend`
+  position: absolute;
+  width: 100%;
+  font-size: smaller;
+`;
+
+const Div = styled.div`
+  display: flex;
+  flex-direction: column;
+`;
+
+const Input = styled.input`
+  width: 100%;
 `;
