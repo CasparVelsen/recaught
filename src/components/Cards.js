e14882d0efbc5ca8660929c8324ba4aa04f6a50b
Caspar Velsen-Zerweck <caspar@velsen-zerweck.de>
2022-03-30 12:19:58 +0200

    Add catch and summary data to cards and update styling of card

--- src/components/Cards.js
+++ src/components/Cards.js
@@ -14,16 +14,60 @@ export default function Cards({ data }) {
       <span>{data.water}</span>
       <span>{data.target}</span>
       <Invisible>
-        <small>{data.stretch}</small>
-        <small>{data.watertemp}</small>
-        <small>{data.watercolor}</small>
-        <small>{data.waterlevel}</small>
-        <small>{data.weather}</small>
-        <small>{data.temperature}</small>
-        <small>{data.airpressure}</small>
-        <small>{data.moon}</small>
-        <small>{data.wind}</small>
-        <small>{data.windspeed}</small>
+        <small>
+          <Term>stretch:</Term> {data.stretch}
+        </small>
+        <small>
+          <Term>watertemp:</Term> {data.watertemp}
+        </small>
+        <small>
+          <Term>watercolor:</Term> {data.watercolor}
+        </small>
+        <small>
+          <Term>waterlevel:</Term> {data.waterlevel}
+        </small>
+        <small>
+          <Term>weather:</Term> {data.weather}
+        </small>
+        <small>
+          <Term>airpressure:</Term> {data.airpressure}
+        </small>
+        <small>
+          <Term>temperature:</Term> {data.temperature}
+        </small>
+        <small>
+          <Term>moon:</Term> {data.moon}
+        </small>
+        <small>
+          <Term>wind:</Term> {data.wind}
+        </small>
+        <small>
+          <Term>windspeed:</Term> {data.windspeed}
+        </small>
+        <small>
+          <Term>species:</Term> {data.species}
+        </small>
+        <small>
+          <Term>bait:</Term> {data.bait}
+        </small>
+        <small>
+          <Term>length:</Term> {data.length}
+        </small>
+        <small>
+          <Term>weight:</Term> {data.weight}
+        </small>
+        <small>
+          <Term>location:</Term> {data.location}
+        </small>
+        <small>
+          <Term>notes:</Term> {data.notes}
+        </small>
+        <small>
+          <Term>total bites:</Term> {data.bites}
+        </small>
+        <small>
+          <Term>lost fish:</Term> {data.lost}
+        </small>
       </Invisible>
     </Card>
   );
@@ -47,9 +91,14 @@ const Div = styled.div`
 const Invisible = styled.div`
   display: flex;
   flex-direction: column;
+  gap: 3px;
   border: 1px solid #ddd;
   padding: 5px;
   margin-top: 10px;
+`;
+
+const Term = styled.span`
+  font-weight: bold;
 `;
 
 const Title = styled.h2`
