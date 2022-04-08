f8d59a2a3a6aac1b114ed28b805fb22471b2526f
Caspar Velsen-Zerweck <caspar@velsen-zerweck.de>
2022-04-08 19:03:50 +0200

    Add delete function for catches

--- src/components/Days-Catches/DisplayCatches.js
+++ src/components/Days-Catches/DisplayCatches.js
@@ -2,9 +2,35 @@ import { useEffect, useState } from 'react';
 import styled from 'styled-components';
 import { GiWeight } from 'react-icons/gi';
 import { BiRuler } from 'react-icons/bi';
+import Button from '../Button';
 
 export default function DisplayCatches() {
   const [catches, setCatches] = useState([]);
+  const [showMore, setShowMore] = useState(true);
+
+  console.log(catches);
+
+  function handleshowMore() {
+    setShowMore(!showMore);
+  }
+
+  async function onDelete(_id) {
+    const filteredCatches = catches.filter(fish => fish._id !== _id);
+    setCatches([
+      {
+        ...catches,
+        filteredCatches,
+      },
+    ]);
+
+    await fetch('api/catches', {
+      method: 'DELETE',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ _id }),
+    });
+  }
 
   useEffect(() => {
     fetch('/api/catches').then(async res => {
@@ -20,22 +46,31 @@ export default function DisplayCatches() {
   return (
     <Catches>
       {catches
-        ? catches.map((data, tempId) => (
+        ? catches.map((data, tempId, _id) => (
             <li key={tempId}>
-              <Card>
-                <Title>{data.species}</Title>
-                <span>
-                  <BiRuler size={30} color={'#687a48'} />
-                  {data.length} cm
-                </span>
-                <span>
-                  <GiWeight size={30} color={'#687a48'} />
-                  {data.weight} kg
-                </span>
+              <Card onClick={handleshowMore}>
+                <div>
+                  <Title>{data.species}</Title>
+                  <span>
+                    <BiRuler size={30} color={'#687a48'} />
+                    {data.length} cm
+                  </span>
+                  <span>
+                    <GiWeight size={30} color={'#687a48'} />
+                    {data.weight} kg
+                  </span>
+                </div>
+                {!showMore && (
+                  <Button
+                    text={'Delete'}
+                    isAccent={true}
+                    onClick={() => onDelete(_id)}
+                  />
+                )}
               </Card>
             </li>
           ))
-        : '...loading cards...'}
+        : 'no catches yet'}
     </Catches>
   );
 }
@@ -55,7 +90,7 @@ const Card = styled.div`
   flex-direction: column;
   justify-content: space-between;
   gap: 5px;
-  padding: 5px;
+  padding: 10px;
   background-color: #fffcf8;
   font-size: 1rem;
   color: #a2c36c;
