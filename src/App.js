dd929c1b249db8f5c8647172c433efab2371b58f
Caspar Velsen-Zerweck <caspar@velsen-zerweck.de>
2022-04-08 11:13:42 +0200

    Final change

--- src/App.js
+++ src/App.js
@@ -11,16 +11,14 @@ export default function App() {
   const navigate = useNavigate();
 
   useEffect(() => {
-    fetch('/api/cards')
-      .then(async res => {
-        const data = await res.json();
-        if (!res.ok) {
-          console.error(data);
-          return [];
-        }
-        return data;
-      })
-      .then(setCards);
+    fetch('/api/cards').then(async res => {
+      const data = await res.json();
+      if (!res.ok) {
+        console.error(data);
+        return [];
+      }
+      setCards([...data]);
+    });
   }, []);
 
   return (
