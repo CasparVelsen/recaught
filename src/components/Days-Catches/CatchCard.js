10073c86e504593ba9da6741dab43acfe555620f
Caspar Velsen-Zerweck <caspar@velsen-zerweck.de>
2022-04-09 00:50:05 +0200

    Changed handleDelete catches for better user experience

---
+++ src/components/Days-Catches/CatchCard.js
@@ -0,0 +1,69 @@
+import { GiWeight } from 'react-icons/gi';
+import { BiRuler } from 'react-icons/bi';
+import Button from '../Button';
+import styled from 'styled-components';
+import { useState } from 'react';
+
+export default function CatchCard({ onDelete, data }) {
+  const [showMore, setShowMore] = useState(true);
+
+  function handleshowMore() {
+    setShowMore(!showMore);
+  }
+
+  return (
+    <Card onClick={handleshowMore}>
+      <Title>{data.species}</Title>
+      <span>
+        <BiRuler size={30} color={'#687a48'} />
+        {data.length} cm
+      </span>
+      {showMore && (
+        <div>
+          <span>
+            <GiWeight size={30} color={'#687a48'} />
+            {data.weight} kg
+          </span>
+        </div>
+      )}
+      {!showMore && (
+        <Button
+          text={'Delete'}
+          isAccent={true}
+          onClick={() => onDelete(data._id)}
+        />
+      )}
+    </Card>
+  );
+}
+
+const Card = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  gap: 5px;
+  padding: 10px;
+  background-color: #fffcf8;
+  font-size: 1rem;
+  color: #a2c36c;
+  border: 0.5px solid #a2c36c;
+  border-radius: 20px;
+  box-shadow: 0px 10px 20px -10px rgba(0, 0, 0, 0.25);
+  width: 100%;
+  height: 110px;
+
+  span {
+    display: flex;
+    align-items: center;
+    justify-content: space-around;
+    text-align: center;
+  }
+`;
+
+const Title = styled.h2`
+  font-size: 1.1rem;
+  padding: 0;
+  margin: 0;
+  color: #687a48;
+  text-align: center;
+`;
--- src/components/Days-Catches/DisplayCatches.js
+++ src/components/Days-Catches/DisplayCatches.js
@@ -1,27 +1,13 @@
 import { useEffect, useState } from 'react';
 import styled from 'styled-components';
-import { GiWeight } from 'react-icons/gi';
-import { BiRuler } from 'react-icons/bi';
-import Button from '../Button';
+import CatchCard from './CatchCard';
 
 export default function DisplayCatches() {
   const [catches, setCatches] = useState([]);
-  const [showMore, setShowMore] = useState(true);
 
-  console.log(catches);
-
-  function handleshowMore() {
-    setShowMore(!showMore);
-  }
-
-  async function onDelete(_id) {
+  async function handleDelete(_id) {
     const filteredCatches = catches.filter(fish => fish._id !== _id);
-    setCatches([
-      {
-        ...catches,
-        filteredCatches,
-      },
-    ]);
+    setCatches(filteredCatches);
 
     await fetch('api/catches', {
       method: 'DELETE',
@@ -45,32 +31,15 @@ export default function DisplayCatches() {
 
   return (
     <Catches>
-      {catches
-        ? catches.map((data, tempId, _id) => (
-            <li key={tempId}>
-              <Card onClick={handleshowMore}>
-                <div>
-                  <Title>{data.species}</Title>
-                  <span>
-                    <BiRuler size={30} color={'#687a48'} />
-                    {data.length} cm
-                  </span>
-                  <span>
-                    <GiWeight size={30} color={'#687a48'} />
-                    {data.weight} kg
-                  </span>
-                </div>
-                {!showMore && (
-                  <Button
-                    text={'Delete'}
-                    isAccent={true}
-                    onClick={() => onDelete(_id)}
-                  />
-                )}
-              </Card>
-            </li>
-          ))
-        : 'no catches yet'}
+      {catches ? (
+        catches.map((data, tempId, _id) => (
+          <li key={tempId}>
+            <CatchCard data={data} onDelete={handleDelete} />
+          </li>
+        ))
+      ) : (
+        <p>no catches yet</p>
+      )}
     </Catches>
   );
 }
@@ -83,34 +52,4 @@ const Catches = styled.ul`
   list-style: none;
   padding: 0;
   width: 100%;
-`;
-
-const Card = styled.div`
-  display: flex;
-  flex-direction: column;
-  justify-content: space-between;
-  gap: 5px;
-  padding: 10px;
-  background-color: #fffcf8;
-  font-size: 1rem;
-  color: #a2c36c;
-  border: 0.5px solid #a2c36c;
-  border-radius: 20px;
-  box-shadow: 0px 10px 20px -10px rgba(0, 0, 0, 0.25);
-  width: 100%;
-
-  span {
-    display: flex;
-    align-items: center;
-    justify-content: space-around;
-    text-align: center;
-  }
-`;
-
-const Title = styled.h2`
-  font-size: 1.1rem;
-  padding: 0;
-  margin: 0;
-  color: #687a48;
-  text-align: center;
 `;
