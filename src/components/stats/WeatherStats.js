11df0e34de67c1355dd02205e94aae461d894d63
Caspar Velsen-Zerweck <caspar@velsen-zerweck.de>
2022-04-19 16:45:56 +0200

    Add conditional rendering for weather stats

--- src/components/stats/WeatherStats.js
+++ src/components/stats/WeatherStats.js
@@ -5,30 +5,61 @@ import Temperature from './charts/Temperatur';
 import Moon from './charts/Moon';
 import Wind from './charts/Wind';
 import WindSpeed from './charts/WindSpeed';
+import { useState } from 'react';
 
 export default function WeatherStats({ filteredCardsByTime }) {
+  const [page, setPage] = useState(0);
+
+  console.log(page);
+
   return (
     <>
       <Select>
-        <option>Weather</option>
-        <option>Temperature</option>
-        <option>Air pressure</option>
-        <option>Moon</option>
-        <option>Wind</option>
-        <option>Wind speed</option>
+        <option onClick={() => setPage(0)}>Weather</option>
+        <option onClick={() => setPage(1)}>Temperature</option>
+        <option onClick={() => setPage(2)}>Air pressure</option>
+        <option onClick={() => setPage(3)}>Moon</option>
+        <option onClick={() => setPage(4)}>Wind</option>
+        <option onClick={() => setPage(5)}>Wind speed</option>
       </Select>
-      <h4>Weather</h4>
-      <Weather filteredCardsByTime={filteredCardsByTime} />
-      <h4>Temperature</h4>
-      <Temperature filteredCardsByTime={filteredCardsByTime} />
-      <h4>Airpressure</h4>
-      <Airpressure filteredCardsByTime={filteredCardsByTime} />
-      <h4>Moon</h4>
-      <Moon filteredCardsByTime={filteredCardsByTime} />
-      <h4>Wind direction</h4>
-      <Wind filteredCardsByTime={filteredCardsByTime} />
-      <h4>Wind speed</h4>
-      <WindSpeed filteredCardsByTime={filteredCardsByTime} />
+      <div>
+        {page === 0 && (
+          <div>
+            <h4>Weather</h4>
+            <Weather filteredCardsByTime={filteredCardsByTime} />
+          </div>
+        )}
+        {page === 1 && (
+          <div>
+            <h4>Temperature</h4>
+            <Temperature filteredCardsByTime={filteredCardsByTime} />
+          </div>
+        )}
+        {page === 2 && (
+          <div>
+            <h4>Airpressure</h4>
+            <Airpressure filteredCardsByTime={filteredCardsByTime} />
+          </div>
+        )}
+        {page === 3 && (
+          <div>
+            <h4>Moon</h4>
+            <Moon filteredCardsByTime={filteredCardsByTime} />
+          </div>
+        )}
+        {page === 4 && (
+          <div>
+            <h4>Wind direction</h4>
+            <Wind filteredCardsByTime={filteredCardsByTime} />
+          </div>
+        )}
+        {page === 5 && (
+          <div>
+            <h4>Wind speed</h4>
+            <WindSpeed filteredCardsByTime={filteredCardsByTime} />
+          </div>
+        )}
+      </div>
     </>
   );
 }
