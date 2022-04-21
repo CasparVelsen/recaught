0b9e08d52dc3bc1787e10bd3941c00a7db533e57
Caspar Velsen-Zerweck <caspar@velsen-zerweck.de>
2022-04-21 15:21:27 +0200

    Add filter function

--- src/components/Days-Catches/DisplayDays.js
+++ src/components/Days-Catches/DisplayDays.js
@@ -1,6 +1,8 @@
 import Cards from './DayCard';
 import styled from 'styled-components';
-import { IoIosArrowDown } from 'react-icons/io';
+import { IoIosArrowDown, IoIosArrowForward } from 'react-icons/io';
+import { useState } from 'react';
+import moment from 'moment';
 
 export default function DisplayDays({
   filteredCards,
@@ -9,18 +11,48 @@ export default function DisplayDays({
   confirmDelete,
   cancelDelete,
 }) {
+  const [showFilterOptions, setShowFilterOptions] = useState(false);
+  const [date, setDate] = useState('');
+
+  const m = moment();
+  const currentYear = m.format('YYYY');
+  const currentMonth = m.format('YYYY[-]MM');
+
+  const filteredCardsByDate = filteredCards.filter(card =>
+    card.date.includes(date)
+  );
+
   return (
     <>
       <Filter>
-        <FilterButton>
-          <IoIosArrowDown color={'#687a48'} />
+        <FilterButton onClick={() => setShowFilterOptions(!showFilterOptions)}>
+          {showFilterOptions && <IoIosArrowForward color={'#687a48'} />}
+          {!showFilterOptions && <IoIosArrowDown color={'#687a48'} />}
           Filter
         </FilterButton>
-        <div></div>
+        {showFilterOptions && (
+          <Options>
+            <div>
+              <Category>Date:</Category>
+              <Together>
+                <input type="radio" onChange={() => setDate('')} />
+                <legend>all</legend>
+              </Together>
+              <Together>
+                <input type="radio" onChange={() => setDate(currentYear)} />
+                <legend>this year</legend>
+              </Together>
+              <Together>
+                <input type="radio" onChange={() => setDate(currentMonth)} />
+                <legend>this month</legend>
+              </Together>
+            </div>
+          </Options>
+        )}
       </Filter>
       <CardsList>
-        {filteredCards
-          ? filteredCards.map((data, tempId) => (
+        {filteredCardsByDate
+          ? filteredCardsByDate.map((data, tempId) => (
               <li key={tempId}>
                 <Cards
                   data={data}
@@ -40,18 +72,42 @@ export default function DisplayDays({
 const Filter = styled.div`
   width: 100%;
   margin: 15px 0;
-`;
-
-const FilterButton = styled.div`
   border: 1px solid #a2c36c;
   border-radius: 10px;
   background-color: #fffcf8;
   color: #687a48;
-  padding: 5px 10px;
+  display: flex;
+  flex-direction: column;
+  gap: 5px;
+  width: 100%;
+  padding-bottom: 10px;
+`;
+
+const FilterButton = styled.div`
   display: flex;
   align-items: center;
   gap: 5px;
-  width: 100%;
+  padding-top: 10px;
+  padding-left: 10px;
+`;
+
+const Options = styled.div`
+  background-color: #fffcf8;
+  color: #687a48;
+  padding: 5px 10px;
+  border-top: 1px solid #a2c36c;
+`;
+
+const Category = styled.div`
+  color: #687a48;
+  font-weight: bold;
+  margin-bottom: 5px;
+`;
+
+const Together = styled.div`
+  display: flex;
+  align-items: center;
+  gap: 5px;
 `;
 
 const CardsList = styled.ul`
